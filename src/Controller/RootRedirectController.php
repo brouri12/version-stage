f253e7665a4025023150c327d5e6ccf442d7d2c8
Fahedmb <fahedmbarek9@mail.com>
2025-08-04 06:57:59 +0200

    Add / → /login redirect controller

--- src/Controller/RootRedirectController.php
+++ src/Controller/RootRedirectController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 final class RootRedirectController extends AbstractController
 {
     #[Route('/', name: 'root_redirect')]
-    public function index(): Response
+    public function __invoke(): Response
     {
         return $this->redirectToRoute('app_login');
     }
